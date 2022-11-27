f5d023afd1280f54563bb8b725f084982d61c89d
Cristhianjo <cristhianbrice16@gmail.com>
2022-11-26 21:27:37 -0500

    Cartera Vencida LWC

--- force-app/main/default/lwc/carteraVencidaLWC/carteraVencidaLWC.js
+++ force-app/main/default/lwc/carteraVencidaLWC/carteraVencidaLWC.js
@@ -1,3 +1,41 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class CarteraVencidaLWC extends LightningElement {}
+
+
+const columns = [
+    { label: 'Credit Card Debt Name ', fieldName: 'Name'},
+    { label: 'Estado ', fieldName: 'Status__c' },
+    { label: 'Producto', fieldName: 'product__c' },
+    { label: 'Monto', fieldName: 'Monto_original__c', type: 'number', typeAttributes:{
+        maximumFractionDigits: '2'
+    } },
+    { label: 'Monto Vencido', fieldName: 'monto_vencido__c', type: 'number', typeAttributes:{
+        maximumFractionDigits: '2'
+    } },
+    { label: 'Fecha emision', fieldName: 'Fecha_emision__c',type: "date",
+    typeAttributes:{
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit"
+    } },
+    { label: 'Fecha Vencimiento ', fieldName: 'Fecha_vencimiento__c', type: "date",
+    typeAttributes:{
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit"
+    } },
+    { label: 'Observacion', fieldName: 'observation__c'},
+    { type: "button", typeAttributes: {  
+        name: 'Select',  
+        title: 'Select',
+        variant: 'brand',  
+        disabled: false,  
+        value: 'Select',  
+        iconPosition: 'center',
+        iconName: 'utility:edit'
+    } }, 
+];
+
+export default class CarteraVencidaLWC extends LightningElement {
+    columns = columns;
+}
